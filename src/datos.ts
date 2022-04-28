8600553a860a985a9f994336cac852ad52bb2d53
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-04-28 18:37:51 -0300

    Initial commit

---
+++ src/datos.ts
@@ -0,0 +1,10 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  //TU CODIGO VA AQUI, POR EJEMPLO:
+  console.log("el dato ingresado es", dato.value);
+  //
+  rotulo.innerHTML = "Ingrese un dato";
+});
